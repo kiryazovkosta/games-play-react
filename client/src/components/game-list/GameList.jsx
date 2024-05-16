47775676091df5df1e753777a31b0b6ea11244d6
kiryazovkosta <kosta.kiryazov@gmail.com>
2024-05-16 16:13:56 +0300

    Add request.js

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+
+import * as gameService from '../../services/gameService';
+
 export default function GameList() {
+    const [games, setGames] = useState([]);
+        
+
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(gameList => setGames(gameList));
+        
+    }, []);
+
+    console.log(games);
+
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,7 +1,9 @@
-const baseUrl = 'http://localhost:3030/jsonstore';
+import { request } from "../api/request";
+
+const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export const  create = async (gameData) => {
-    const response = await fetch(`${baseUrl}/games`, {
+    const response = await fetch(baseUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -11,4 +13,9 @@ export const  create = async (gameData) => {
 
     const result = await response.json();
     return result;
+}
+
+export const getAll = async() => {
+    const result = await request('GET', baseUrl);
+    return Object.values(result);
 }
